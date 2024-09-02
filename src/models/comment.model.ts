61588290837e58850c52e7f5626be591a56a058c
Sgutierrez24 <SamuelGutierrezD24@gmail.com>
2024-09-02 11:13:28 -0500

    index change

--- src/models/comment.model.ts
+++ src/models/comment.model.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export interface CommentInput {
     text: string,
     email: string,
+    commentId: string,
     responses: string[],
     reactions: string[]
 }
@@ -17,6 +18,7 @@ export interface CommentDocument extends CommentInput, mongoose.Document{
 const commentSchema = new mongoose.Schema({
     text: {type: String, required: true},
     email: {type: String, required: false},
+    commentId: {type: String, required: false},
     responses: [{ type: String, required: false }],
     reactions: [{ type: String, required: false }],
 }, {timestamps: true, collection: "comments"});
